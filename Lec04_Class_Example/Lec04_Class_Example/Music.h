987e80bb612af2c1e19f6aa231f6f86f2e6aa995
hcb3 <hcb4689@gmail.com>
2026-04-08 12:20:02 +0900

    file added

--- Lec04_Class_Example/Lec04_Class_Example/Music.h
+++ Lec04_Class_Example/Lec04_Class_Example/Music.h
@@ -52,9 +52,20 @@ public:
 			if (musicList[i].getTitle() == title)
 				return &musicList[i];
 
-		}	
+		}
 		return NULL;
 	}
+	//Artist로 검색하기
+	vector<Music*> searchByArtist(string artist) {
+		vector <Music*> result;
+		//musicList에 있는 artist와 입력 artist가 같으면 result에 push_back
+		for (int i = 0; i < musicList.size(); i++) {
+			if (musicList[i].getArtist() == artist)
+				result.push_back(&musicList[i]);
+		}
+		return result;
+	}
+
 
 
 };
