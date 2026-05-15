f88df3a4aa36081474b372150de072d09f11735f
hcb3 <hcb4689@gmail.com>
2026-05-15 10:43:50 +0900

    file added

---
+++ Lec10_Graph/Lec10_Graph/AdjMatGraph.h
@@ -0,0 +1,69 @@
+#pragma once
+#define MAX_VTXS 20
+#include <stdio.h>
+
+class AdjMatGraph {
+protected:
+	int size;
+	char vertices[MAX_VTXS];
+	int adj[MAX_VTXS][MAX_VTXS];
+public:
+	AdjMatGraph() { reset(); }
+	char getVertex(int i) { return vertices[i]; }
+	int getEdge(int i, int j) { return adj[i][j]; }
+	void setEdge(int i, int j, int val) { adj[i][j] = val; }
+	bool isEmpty() { return size == 0; }
+	bool isFull() { return size >= MAX_VTXS; }
+
+	// 그래프 초기화 ==> 공백 상태의 그래프
+	void reset() {
+		size = 0;
+		for (int i = 0; i < MAX_VTXS; i++)
+			for (int j = 0; j < MAX_VTXS; j++)
+				setEdge(i, j, 0);
+	}
+	//정점삽입
+	void insertVertex(char name) {
+		if (!isFull()) vertices[size++] = name;
+		else printf("Error: 그래프 정점 개수 초과\n");
+	}
+
+	void insertEdge(int u, int v) {
+		setEdge(u, v, 1);
+		setEdge(v, u, 1);
+	}
+
+	void display(FILE* fp = stdout) {
+	fprintf(fp, "% d\n", size);
+	for (int i = 0; i < size; i++) {
+		fprintf(fp, "%c", getVertex(i));
+		for (int j = 0 ; j < size ; j++)
+		fprintf(fp, "%3d", getEdge(i, j));
+		fprintf(fp, "\n");
+		}
+	}
+};
+
+class SrchAMGraph : public AdjMatGraph
+{
+	bool visited[MAX_VTXS];
+public:
+	void resetVisited() {
+		for (int i = 0; i < size; i++)
+			visited[i] = false;
+	}
+	bool isLinked(int u, int v) { return getEdge(u, v) != 0; }
+	
+	//깊이 우선 탐색 함수
+	void DFS(int v) {
+		visited[v] = true;
+		printf("%c", getVertex(v));
+
+		for (int w = 0; w < size; w++)
+			if (isLinked(v, w) && visited[w] == false)
+				DFS(w);
+
+	}
+
+};
+
